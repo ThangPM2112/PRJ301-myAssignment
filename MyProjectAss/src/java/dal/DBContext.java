8f1a366006bf24672cd872d6689cc8035859fdd6
ThangPM2112 <115055462+ThangPM2112@users.noreply.github.com>
2022-10-28 00:53:50 +0700

    ok update

--- MyProjectAss/src/java/dal/DBContext.java
+++ MyProjectAss/src/java/dal/DBContext.java
@@ -1,13 +1,35 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package dal;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author Manh Thang
+ * @author Admin
  */
 public class DBContext {
-    
+
+    protected Connection connection;
+    public DBContext() {
+        try {
+            String user = "sa";
+            String pass = "12345";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=projectCheckAttend";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("ok");
+        } catch (Exception ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public static void main(String[] args) {
+        DBContext bContext = new DBContext();
+    }
 }
